12594fdcf6e540054f1acac3443c4faf0dca3593
JuanpaDionicio <juan.dionicio@triplei.mx>
2020-10-08 15:41:39 -0500

    Add props not closed div content

--- dist/components/ConfirmModal.js
+++ dist/components/ConfirmModal.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports["default"] = void 0;
 
@@ -11,63 +11,102 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _reactstrap = require("reactstrap");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule
+    ? obj
+    : {
+        default: obj,
+      };
+}
 
 var ConfirmModal = function ConfirmModal(_ref) {
   var onClose = _ref.onClose,
-      message = _ref.message,
-      title = _ref.title,
-      confirmText = _ref.confirmText,
-      cancelText = _ref.cancelText,
-      confirmColor = _ref.confirmColor,
-      cancelColor = _ref.cancelColor,
-      className = _ref.className,
-      buttonsComponent = _ref.buttonsComponent,
-      size = _ref.size;
+    message = _ref.message,
+    title = _ref.title,
+    confirmText = _ref.confirmText,
+    cancelText = _ref.cancelText,
+    confirmColor = _ref.confirmColor,
+    cancelColor = _ref.cancelColor,
+    className = _ref.className,
+    buttonsComponent = _ref.buttonsComponent,
+    size = _ref.size;
 
-  var buttonsContent = _react["default"].createElement(_react["default"].Fragment, null, cancelText && _react["default"].createElement(_reactstrap.Button, {
-    color: cancelColor,
-    onClick: function onClick() {
-      return onClose(false);
-    }
-  }, cancelText), ' ', _react["default"].createElement(_reactstrap.Button, {
-    color: confirmColor,
-    onClick: function onClick() {
-      return onClose(true);
-    }
-  }, confirmText));
+  var buttonsContent = _react["default"].createElement(
+    _react["default"].Fragment,
+    null,
+    cancelText &&
+      _react["default"].createElement(
+        _reactstrap.Button,
+        {
+          color: cancelColor,
+          onClick: function onClick() {
+            return onClose(false);
+          },
+        },
+        cancelText
+      ),
+    " ",
+    _react["default"].createElement(
+      _reactstrap.Button,
+      {
+        color: confirmColor,
+        onClick: function onClick() {
+          return onClose(true);
+        },
+      },
+      confirmText
+    )
+  );
 
   if (buttonsComponent) {
     var CustomComponent = buttonsComponent;
     buttonsContent = _react["default"].createElement(CustomComponent, {
-      onClose: onClose
+      onClose: onClose,
     });
   }
 
-  return _react["default"].createElement(_reactstrap.Modal, {
-    size: size,
-    isOpen: true,
-    toggle: function toggle() {
-      return onClose(false);
+  return _react["default"].createElement(
+    _reactstrap.Modal,
+    {
+      size: size,
+      isOpen: true,
+      toggle: function toggle() {
+        return onClose(false);
+      },
+      className: "reactstrap-confirm ".concat(className),
     },
-    className: "reactstrap-confirm ".concat(className)
-  }, title && _react["default"].createElement(_reactstrap.ModalHeader, {
-    toggle: function toggle() {
-      return onClose(false);
-    }
-  }, title || null), _react["default"].createElement(_reactstrap.ModalBody, null, message), _react["default"].createElement(_reactstrap.ModalFooter, null, buttonsContent));
+    title &&
+      _react["default"].createElement(
+        _reactstrap.ModalHeader,
+        {
+          toggle: function toggle() {
+            if (_ref.closedforContent) {
+              return onClose(false);
+            }
+          },
+        },
+        title || null
+      ),
+    _react["default"].createElement(_reactstrap.ModalBody, null, message),
+    _react["default"].createElement(
+      _reactstrap.ModalFooter,
+      null,
+      buttonsContent
+    )
+  );
 };
 
 ConfirmModal.defaultProps = {
-  message: 'Are you sure?',
-  title: 'Warning!',
-  confirmText: 'Ok',
-  cancelText: 'Cancel',
-  confirmColor: 'primary',
-  cancelColor: '',
-  className: '',
+  message: "Are you sure?",
+  title: "Warning!",
+  confirmText: "Ok",
+  cancelText: "Cancel",
+  confirmColor: "primary",
+  cancelColor: "",
+  className: "",
   buttonsComponent: null,
-  size: null
+  size: null,
+  closedforContent: true,
 };
 ConfirmModal.propTypes = {
   onClose: _propTypes["default"].func.isRequired,
@@ -79,7 +118,8 @@ ConfirmModal.propTypes = {
   cancelColor: _propTypes["default"].string,
   className: _propTypes["default"].string,
   size: _propTypes["default"].string,
-  buttonsComponent: _propTypes["default"].func
+  buttonsComponent: _propTypes["default"].func,
+  closedforContent: _propTypes["default"].bool,
 };
 var _default = ConfirmModal;
 exports["default"] = _default;
--- dist/index.js
+++ dist/index.js
@@ -11,9 +11,29 @@ var _reactDom = require("react-dom");
 
 var _ConfirmModal = _interopRequireDefault(require("./components/ConfirmModal"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    "default": obj
+  };
+}
 
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
 
 var confirm = function confirm(props) {
   return new Promise(function (resolve) {
